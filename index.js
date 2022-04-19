c6d2096f67fa3ad50dea0763fc9138974c0fad25
Kaegan O'Hara <kohara321@gmail.com>
2022-04-19 17:56:46 -0600

    commit tree 3e26f68915789aea5285dc4b6e46f19333a0260f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Broom) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat(Arnold) {
+    const addCats = ["Arnold", ...cats];
+    return addCats
+}
+
+function removeLastCat() {
+    const lessCats = cats.slice(0, cats.length -1);
+    return lessCats;
+}
+
+function removeFirstCat() {
+    const lostCats = cats.slice(1);
+    return lostCats;
+}
+
